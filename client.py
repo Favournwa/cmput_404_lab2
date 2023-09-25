2c25510c6c9be40017430a2c2cc1adb11b856e71
Favournwa <nwachukw@ualberta.ca>
2023-09-24 18:57:59 -0600

    client.py is working now

--- client.py
+++ client.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 import socket
 
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(("www.google.com", 8080))
+        s.connect(("www.google.com", 80))
         s.sendall(b"GET / HTTP/1.0\r\nHost:www.google.com\r\n\r\n")
         s.shutdown(socket.SHUT_WR)
-        result = s.recv(4096)
-        while(len(result)>0):
-            print(result)
-            result = s.recv(4096)
-        #full_data = b""
-        #while True:
-        #    data = s.recv(4096)
-        #    if not data:
-        #        break
-        #    full_data += data
-        #print(full_data)
+        response = b""
+        while True:
+            chunk = s.recv(4096)
+            if len(chunk) == 0:
+                break
+            response = response + chunk
+        print(response)
 
 
 if __name__ == "__main__":
